e9a9351f1e360415704e253502fb9e4b86b55191
c2h31032 <c2h31032@bunkyo.ac.jp>
2024-09-24 23:47:49 +0900

    Delete m5-4_table_drop.php

--- m5-4_table_drop.php
+++
@@ -1,9 +0,0 @@
-<?php
- $dsn = 'mysql:dbname=tb260047db;host=localhost';
-    $user = 'tb-260047';
-    $password = 'Tma6WSdybd';
-    $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
- 
-    $sql = 'DROP TABLE mission5_4';
-    $stmt = $pdo->query($sql);
-    ?>
